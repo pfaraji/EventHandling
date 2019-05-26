0e6ecdbbad31080cdae34ceb85a7600deb7556e5
Parvin Faraji <47976214+pfaraji@users.noreply.github.com>
2019-05-26 11:43:54 +0430

    Update Bundleconfig.css
    Update Bundleconfig.css

--- EventHandlingApp/EventHandlingApp/App_Start/BundleConfig.cs
+++ EventHandlingApp/EventHandlingApp/App_Start/BundleConfig.cs
@@ -17,7 +17,6 @@ namespace EventHandlingApp
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
                         "~/models/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
